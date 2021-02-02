7aa15d0b1acd08ca5bb093bef807ff778695ea59
Andre Lucyo <andre.lucyo2@gmail.com>
2021-02-02 00:53:02 -0300

    Adicionado Slick, com links das dependencias direto da internet. ver http://kenwheeler.github.io/slick/  Video 4 - 19:55 (https://www.youtube.com/watch?v=IA_fwt152AI&list=PL4iwH9RF8xHmjxVNJcyNSkVDYNUo6r2BM&index=4)

---
+++ js/main.js
@@ -0,0 +1,6 @@
+/*
+ opção sigle item
+ ver http://kenwheeler.github.io/slick/ 
+ Video 4 - 19:55 (https://www.youtube.com/watch?v=IA_fwt152AI&list=PL4iwH9RF8xHmjxVNJcyNSkVDYNUo6r2BM&index=4)
+*/
+$('.slider-principal').slick();
